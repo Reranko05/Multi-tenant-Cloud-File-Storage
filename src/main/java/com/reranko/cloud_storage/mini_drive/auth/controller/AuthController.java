015024f1294406dd8068198f1822cc0a748b3b27
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-01-24 14:29:22 +0530

    Add AuthController Skeleton for registration

---
+++ src/main/java/com/reranko/cloud_storage/mini_drive/auth/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.reranko.cloud_storage.mini_drive.config;
+
+import org.reranko.cloud_storage.mini_drive.auth.dto.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/* AuthController just a middleman that receives HTTP requests from RegisterRequest and sends back a response.
+ * It does not any verification or processing logic for the data, it simply acknowledges the receipt of the registration request.
+ */
+
+@RestController // This class listens for HTTP reuests and sends back HTTP responses
+@RequestMapping("/auth") // All URLs in this controller will start with /auth
+public class AuthController {
+
+    @PostMapping("/register") // When someone sends a POST request to /auth/register, this method is called
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) { // Take the JSON sent by the user and put it into a Java object
+        return ResponseEntity.ok("Registration request received"); // Send back a simple response saying we got the request
+    }
+}
