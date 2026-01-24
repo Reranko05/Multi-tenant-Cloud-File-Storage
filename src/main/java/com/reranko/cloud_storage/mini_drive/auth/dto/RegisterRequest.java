637e765007c2d5fd0cdab36fdb6d283761f49777
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-01-24 14:07:26 +0530

    Created User Registration Class

---
+++ src/main/java/com/reranko/cloud_storage/mini_drive/auth/dto/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.reranko.cloud_storage.mini_drive.config;
+
+public class RegisterRequest {
+
+    private String email;
+    private String password;
+
+    /* Getter read data
+     * Setter write data
+     * Constructors create objects
+     */
+
+    public RegisterRequest() {  // Default constructor used to crete an empty object
+    }
+
+    public String getEmail() {  // Returns the email value by the user
+        return email; 
+    }
+
+    public void setEmail(String email) { // Sets the email value from the incoming request data
+        this.email = email;
+    }
+
+    public String getPassword() {  // Returns the password value by the user
+        return password;
+    }
+
+    public void setPassword(String password) { // Sets the password value from the incoming request data
+        this.password = password;
+    }
+}
