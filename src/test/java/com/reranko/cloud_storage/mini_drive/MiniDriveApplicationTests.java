22bfbf3151a10cd4a1abc6fe4b8cd26b5db47ef8
Aaditya Srinivasan <aadityasri03@gmail.com>
2026-01-24 13:26:34 +0530

    Initial Commit

---
+++ src/test/java/com/reranko/cloud_storage/mini_drive/MiniDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reranko.cloud_storage.mini_drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
